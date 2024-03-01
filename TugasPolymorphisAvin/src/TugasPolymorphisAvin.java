3165dd1eb250015a2ebeb43512269751c5b3c54a
alvin laia <140680069+alvinuslaia@users.noreply.github.com>
2024-03-01 12:39:56 +0700

    Add files via upload

---
+++ TugasPolymorphisAvin/src/TugasPolymorphisAvin.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author ACER
+ */
+public class TugasPolymorphisAvin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bunga Bg = new Bunga();
+        
+        BungaMawar  Bm= new BungaMawar();
+        Melati Ml= new Melati ();
+        Matahari Mr= new Matahari ();
+        Anggrek Ag= new Anggrek ();
+        
+        Bg.Warna();
+        Bm.Warna();
+        Mr.Warna();
+        Ml.Warna();
+        Ag.Warna();
+    }
+    
+    }
+    
+
